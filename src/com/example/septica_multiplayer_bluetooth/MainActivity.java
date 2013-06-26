9b353479d89c9a9f1911de098cf04fc9f8137872
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2013-06-26 09:35:51 +0300

    Graphical interface - MainActivity

--- src/com/example/septica_multiplayer_bluetooth/MainActivity.java
+++ src/com/example/septica_multiplayer_bluetooth/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.septica_multiplayer_bluetooth;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +13,19 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button b_multiplayer = (Button) findViewById(R.id.b_multiplayer);
+        Button b_start = (Button) findViewById(R.id.b_start);
+        Button b_exit = (Button) findViewById(R.id.b_exit);
+        
+        b_exit.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				finish();
+			}
+		});
     }
 
 
