9df1a50b72943fafb3924ac00989fbc29c304935
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2013-09-13 14:12:52 +0300

    Fullscreen.

--- src/com/example/septica_multiplayer_bluetooth/GameActivity.java
+++ src/com/example/septica_multiplayer_bluetooth/GameActivity.java
@@ -1,17 +1,21 @@
 package com.example.septica_multiplayer_bluetooth;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import com.example.game.Game.GameSheet;
 
 public class GameActivity extends Activity
 {
+	GameSheet gameSheet;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_game);
+		gameSheet = new GameSheet(this);
+		setContentView(gameSheet);
 	}
 
 	@Override
